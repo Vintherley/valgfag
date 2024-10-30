29a5a8e145f5a495ffde2a5a92a3739bd4fed071
Shoelle123 <153712509+Shoelle123@users.noreply.github.com>
2024-10-30 09:57:34 +0100

    Opdateret Login.vue og tilføjet ændringer i App.vue, HomeView.vue og firebase.js

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,39 @@
-<script setup>
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firebaseConfig = {
+<template>
+    <div>
+      <h2>Login</h2>
+      <input type="text" v-model="username" placeholder="Brugernavn" />
+      <input type="password" v-model="password" placeholder="Kodeord" />
+      <button @click="login">Login</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { initializeApp } from 'firebase/app';
+  import { getFirestore } from 'firebase/firestore';
+  
+  const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
     projectId: "YOUR_PROJECT_ID",
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
     messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-</script>
-
-<template>
-
-</template>
-
-<style>
-
-</style>
+    appId: "YOUR_APP_ID",
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+  const username = ref('');
+  const password = ref('');
+  
+  const login = () => {
+    if (username.value === 'Charlotte' && password.value === '123V22pry') {
+      alert('Login successful!');
+    } else {
+      alert('Incorrect username or password.');
+    }
+  };
+  </script>
+  
+  
